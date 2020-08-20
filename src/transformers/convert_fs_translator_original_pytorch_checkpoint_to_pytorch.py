257597a9542120c3971a3459cc99133275a50f5d
Stas Bekman <stas@stason.org>
2020-08-19 19:19:52 -0700

    parametrize convert script

--- src/transformers/convert_fs_translator_original_pytorch_checkpoint_to_pytorch.py
+++ src/transformers/convert_fs_translator_original_pytorch_checkpoint_to_pytorch.py
@@ -16,7 +16,7 @@
 
 # exec:
 # cd /code/huggingface/transformers-fair-wmt
-# PYTHONPATH="src" python src/transformers/convert_fs_translator_original_pytorch_checkpoint_to_pytorch.py --fairseq_transformer_checkpoint_path data/wmt19.ru-en.ensemble --pytorch_dump_folder_path data/wmt19-ru-en
+# PYTHONPATH="src" python src/transformers/convert_fs_translator_original_pytorch_checkpoint_to_pytorch.py --fairseq_transformer_checkpoint_path data/wmt19.ru-en.ensemble --pytorch_dump_folder_path data/wmt19-ru-en --src_lang ru --tgt_lang en
 
 import argparse
 import json
@@ -45,16 +45,14 @@ def rewrite_dict_keys(d):
     # d = {'le@@': 5, 'tt@@': 6, 'er': 7} => {'le': 5, 'tt': 6, 'er</w>': 7}
     d2=dict((re.sub(r'@@$', '', k), v) if k.endswith('@@') else (re.sub(r'$', '</w>', k), v) for k, v in d.items())
     keep_keys = "<s> <pad> </s> <unk>".split()
+    # restore the special tokens
     for k in keep_keys:
         del d2[f"{k}</w>"]
         d2[k] = d[k] # restore
     return d2
     #return dict((re.sub(r'@@', '</w>', k, 0, re.M), v) if k.endswith('@@') else (k, v) for k, v in d.items())
 
-def convert_fairseq_transformer_checkpoint_to_pytorch(fairseq_transformer_checkpoint_path, pytorch_dump_folder_path):
-
-    src_lang = 'ru'
-    tgt_lang = 'en'
+def convert_fairseq_transformer_checkpoint_to_pytorch(fairseq_transformer_checkpoint_path, pytorch_dump_folder_path, src_lang, tgt_lang):
 
     vocab_file = os.path.join(pytorch_dump_folder_path, VOCAB_FILES_NAMES["vocab_file"])
     merge_file = os.path.join(pytorch_dump_folder_path, VOCAB_FILES_NAMES["merges_file"])
@@ -62,6 +60,8 @@ def convert_fairseq_transformer_checkpoint_to_pytorch(fairseq_transformer_checkp
     # prep
     assert os.path.exists(fairseq_transformer_checkpoint_path)
     os.makedirs(pytorch_dump_folder_path, exist_ok=True)
+
+    print(f"Writing results to {pytorch_dump_folder_path}")
 
     # done:
     # convert dicts
@@ -72,6 +72,7 @@ def convert_fairseq_transformer_checkpoint_to_pytorch(fairseq_transformer_checkp
     src_dict = Dictionary.load(src_dict_file)
     src_vocab = rewrite_dict_keys(src_dict.indices)
     pytorch_vocab_file_src = os.path.join(pytorch_dump_folder_path, f"vocab-{src_lang}.json")
+    print(f"Generating {pytorch_vocab_file_src}")
     with open(pytorch_vocab_file_src, "w", encoding="utf-8") as f:
         f.write(json.dumps(src_vocab, ensure_ascii=False, indent=2))
 
@@ -79,6 +80,7 @@ def convert_fairseq_transformer_checkpoint_to_pytorch(fairseq_transformer_checkp
     tgt_dict = Dictionary.load(tgt_dict_file)
     tgt_vocab = rewrite_dict_keys(tgt_dict.indices)
     pytorch_vocab_file_tgt = os.path.join(pytorch_dump_folder_path, f"vocab-{tgt_lang}.json")
+    print(f"Generating {pytorch_vocab_file_tgt}")
     with open(pytorch_vocab_file_tgt, "w", encoding="utf-8") as f:
         f.write(json.dumps(tgt_vocab, ensure_ascii=False, indent=2))
 
@@ -90,6 +92,7 @@ def convert_fairseq_transformer_checkpoint_to_pytorch(fairseq_transformer_checkp
     # not needed, already uses </w>
     # re.sub(r'@@', '</w>', merges, 0, re.M) # @@ => </w>
     merges = re.sub(r' \d+$', '', merges, 0, re.M)  # remove frequency number
+    print(f"Generating {merge_file}")
     with open(merge_file, "w", encoding="utf-8") as fout:
        fout.write(merges)
     # reversed:
@@ -104,6 +107,7 @@ def convert_fairseq_transformer_checkpoint_to_pytorch(fairseq_transformer_checkp
     # config - XXX: what really should go there, other than "do_lower_case": False,
     fairseq_config_file = os.path.join(pytorch_dump_folder_path, "config.json")
     conf = {"do_lower_case": False, "model_max_length": 1024}
+    print(f"Generating {fairseq_config_file}")
     with open(fairseq_config_file, "w", encoding="utf-8") as f:
         f.write(json.dumps(conf, ensure_ascii=False, indent=None))
 
@@ -128,5 +132,11 @@ if __name__ == "__main__":
     parser.add_argument(
         "--pytorch_dump_folder_path", default=None, type=str, required=True, help="Path to the output PyTorch model."
     )
+    parser.add_argument(
+        "--src_lang", default=None, type=str, required=True, help="Source language (ru/de/en/fr)."
+    )
+    parser.add_argument(
+        "--tgt_lang", default=None, type=str, required=True, help="Target language."
+    )
     args = parser.parse_args()
-    convert_fairseq_transformer_checkpoint_to_pytorch(args.fairseq_transformer_checkpoint_path, args.pytorch_dump_folder_path)
+    convert_fairseq_transformer_checkpoint_to_pytorch(args.fairseq_transformer_checkpoint_path, args.pytorch_dump_folder_path, args.src_lang, args.tgt_lang)
